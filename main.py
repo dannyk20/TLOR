45b66a171771c63126793eab09677d928db9ea76
JungUiJin <yealkki38@gmail.com>
2023-11-15 16:39:39 +0900

    JungUiJin

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def count_elements(my_list):
+    return len(my_list)
+
+# 예제 사용법
+example_list = [1, 2, 3, 4, 5]
+result = count_elements(example_list)
+print(f"The number of elements in the list is: {result}")
+
