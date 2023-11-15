4eaade4ab26df33d79f415bc2340a962d4730fc1
KimGyeongHo <desperado9889@gmail.com>
2023-11-15 17:08:38 +0900

    modify main.py KimGyeongHo

--- main.py
+++ main.py
@@ -10,16 +10,3 @@ else:
 
 def add_numbers(a, b):
     return a + b
-
-# 예제 사용법
-num1 = 5
-num2 = 10
-result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is: {result}")
-
-def to_uppercase(input_string):
-    return input_string.upper()
-
-original_string = "Hello, World!"
-uppercase_string = to_uppercase(original_string)
-print(uppercase_string)
